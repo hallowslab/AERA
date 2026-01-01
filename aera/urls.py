25b7d1d46f37da071eae7f5b9321d9c4f99ed36c
hallowslab <hallow.tech.lab@gmail.com>
2026-01-01 23:58:08 +0000

    aera index (WIP)

--- aera/urls.py
+++ aera/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name="aera"
+
 urlpatterns = [
     path("", views.index, name="aera_home"),
 ]
--- aera/views.py
+++ aera/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello from ÆRA App!")
+    return render(request, "aera_index.html")
