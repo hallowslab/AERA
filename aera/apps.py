ef540061a8770c77a5c99d1ba4f6e2923d430485
hallowslab <hallow.tech.lab@gmail.com>
2026-01-24 22:45:26 +0000

    update AppConfig

--- aera/apps.py
+++ aera/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AeraConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "aera"
+    verbose_name = "ÆRA"
+    is_modular = True
